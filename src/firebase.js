207951e8e6e494ab4df4cfa80f7fb95821305366
Vistrit <56611775+VistritPandey@users.noreply.github.com>
2022-01-29 13:49:31 +0530

    k

--- src/firebase.js
+++ src/firebase.js
@@ -13,7 +13,7 @@ const firebaseConfig = {
   const firebaseApp = firebase.initializeApp(firebaseConfig);
   const db = firebaseApp.firestore();
   const auth = firebase.auth();
-  var provider = new firebase.auth.GoogleAuthProvider();
+  const provider = new firebase.auth.GoogleAuthProvider();
   
   export { auth, provider };
   export default db;
