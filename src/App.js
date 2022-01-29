5f4b4bb2b002faecfbc90c305c7f91700c4d09a7
Vistrit <56611775+VistritPandey@users.noreply.github.com>
2022-01-29 10:01:33 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { Route, BrowserRouter, Switch } from "react-router-dom";
+import "./App.css";
+import Chat from "./Chat";
+import Login from "./Login";
+import Sidebar from "./Sidebar";
+import { useStateValue } from "./StateProvider";
+
+function App() {
+  const [{ user }, dispatch] = useStateValue();
+
+  return (
+    // BEM naming convention
+    <div className="app">
+      {!user ? (
+        <Login />
+      ) : (
+        <div className="app__body">
+          <BrowserRouter>
+            <Sidebar />
+            <Switch>
+              <Route path="/rooms/:roomId">
+                <Chat />
+              </Route>
+              <Route path="/">
+                <div className="app__body-emptyChat">
+                </div>
+              </Route>
+            </Switch>
+          </BrowserRouter>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+      // use your own config file
+  };
+
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+  const db = firebaseApp.firestore();
+  const auth = firebase.auth();
+  var provider = new firebase.auth.GoogleAuthProvider();
+  
+  export { auth, provider };
+  export default db;
